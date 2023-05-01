337ae7e0b13eb3488961e68fa193bc943327dc86
Junior <jos.pd2019@gmail.com>
2023-05-01 18:32:52 -0300

    Starting EnterpriseModule Tests

---
+++ lib/app/utils/enterprise_test_objects/test_objects.dart
@@ -0,0 +1,43 @@
+import 'package:cash_helper_app/app/modules/enterprise_module/domain/entities/enterprise_entity.dart';
+import 'package:cash_helper_app/app/modules/enterprise_module/infra/models/enterprise_model.dart';
+
+class EnterpriseTestObjects {
+  static Map<String, dynamic> enterpriseMap = {
+    'enterpriseId': "enterpriseId",
+    'enterpriseCnpj': "00000000000122",
+    'enterpriseName': "PrimeSoftware",
+    'enterpriseEmail': "prime@email.com",
+    'enterprisePhoneNumber': "3835274625",
+    'enterpriseCode': "537ujs02k",
+    'enterpriseCity': "Turmalina",
+    'enterpriseCep': "39660000",
+    'enterpriseState': "Minas Gerais",
+    'enterpriseAddressStreet': "Andorinhas",
+    'enterpriseAddressNumber': 381
+  };
+  static final enterpriseModel = EnterpriseModel(
+      enterpriseId: "enterpriseId",
+      enterpriseCnpj: "00000000000122",
+      enterpriseName: "PrimeSoftware",
+      enterpriseEmail: "prime@email.com",
+      enterprisePhoneNumber: "3835274625",
+      enterpriseCode: "537ujs02k",
+      enterpriseCity: "Turmalina",
+      enterpriseCep: "39660000",
+      enterpriseState: "Minas Gerais",
+      enterpriseAddressStreet: "Andorinhas",
+      enterpriseAddressNumber: 381);
+      
+  static final enterpriseEntity = EnterpriseEntity(
+      enterpriseId: "enterpriseId",
+      enterpriseCnpj: "00000000000122",
+      enterpriseName: "PrimeSoftware",
+      enterpriseEmail: "prime@email.com",
+      enterprisePhoneNumber: "3835274625",
+      enterpriseCode: "537ujs02k",
+      enterpriseCity: "Turmalina",
+      enterpriseCep: "39660000",
+      enterpriseState: "Minas Gerais",
+      enterpriseAddressStreet: "Andorinhas",
+      enterpriseAddressNumber: 381);
+}
---
+++ test/app/modules/enterprise_module/external/enterprise_database_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('enterprise database ...', () async {
+    // TODO: Implement test
+  });
+}
