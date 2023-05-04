44dba56cf862ae983e9d9c4bea0cba698b72cdbb
Junior <jos.pd2019@gmail.com>
2023-05-03 21:31:29 -0300

    Usecase GetEnterpriseByCode Tested

--- lib/app/modules/enterprise_module/domain/usecases/get_enterprise_by_code/get_enterprise_by_code.dart
+++ lib/app/modules/enterprise_module/domain/usecases/get_enterprise_by_code/get_enterprise_by_code.dart
@@ -1,6 +1,8 @@
-import 'package:cash_helper_app/app/modules/enterprise_module/domain/entities/enterprise_entity.dart';
 import 'package:cash_helper_app/app/modules/enterprise_module/domain/usecases/get_enterprise_by_code/iget_enterprise_by_code.dart';
 import 'package:cash_helper_app/app/modules/enterprise_module/infra/data/enterprise_repository.dart';
+
+import '../../../infra/models/enterprise_model.dart';
+import '../../entities/enterprise_entity.dart';
 
 class GetEnterpriseByCode implements IGetEnterpriseByCode {
   GetEnterpriseByCode({required EnterpriseRepository repository})
@@ -8,8 +10,8 @@ class GetEnterpriseByCode implements IGetEnterpriseByCode {
 
   final EnterpriseRepository _repository;
   @override
-  Future? call(EnterpriseEntity enterpriseEntity) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<EnterpriseEntity>? call(String enterpriseCode) async {
+    final enterprise = await _repository.getEnterpriseByCode(enterpriseCode);
+    return EnterpriseModel.toEntityData(enterprise);
   }
 }
--- test/app/modules/enterprise_module/domain/usecases/get_enterprise_by_code/get_enterprise_by_code_test.dart
+++ test/app/modules/enterprise_module/domain/usecases/get_enterprise_by_code/get_enterprise_by_code_test.dart
@@ -1,7 +1,12 @@
 import 'package:cash_helper_app/app/modules/enterprise_module/domain/entities/enterprise_entity.dart';
 import 'package:cash_helper_app/app/modules/enterprise_module/domain/usecases/get_enterprise_by_code/iget_enterprise_by_code.dart';
 import 'package:cash_helper_app/app/modules/enterprise_module/infra/data/enterprise_repository.dart';
+import 'package:cash_helper_app/app/modules/enterprise_module/infra/models/enterprise_model.dart';
+import 'package:cash_helper_app/app/utils/enterprise_test_objects/test_objects.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../../mocks/mocks.dart';
 
 class GetEnterpriseByCodeMock implements IGetEnterpriseByCode {
   GetEnterpriseByCodeMock({required EnterpriseRepository repository})
@@ -9,13 +14,27 @@ class GetEnterpriseByCodeMock implements IGetEnterpriseByCode {
 
   final EnterpriseRepository _repository;
   @override
-  Future? call(EnterpriseEntity enterpriseEntity) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<EnterpriseEntity>? call(String enterpriseCode) async {
+    final enterprise = await _repository.getEnterpriseByCode(enterpriseCode);
+    return EnterpriseModel.toEntityData(enterprise);
   }
 }
+
 void main() {
-  testWidgets('get enterprise by code ...', (tester) async {
-    // TODO: Implement test
+  late EnterpriseRepoMock repository;
+  late GetEnterpriseByCodeMock usecase;
+  setUp(() {
+    repository = EnterpriseRepoMock();
+    usecase = GetEnterpriseByCodeMock(repository: repository);
   });
+  test(
+    'GetEnterpriseByCode Function should Call Repository to get an enterprise object',
+    () async {
+    when(repository.getEnterpriseByCode(any))
+          .thenAnswer((_) async => EnterpriseTestObjects.enterpriseModel);
+      final enterprise = await usecase("enterpriseCode");
+      expect(enterprise, isA<EnterpriseEntity>());
+      expect(enterprise != null, equals(true));
+    },
+  );
 }
