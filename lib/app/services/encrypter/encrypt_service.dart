c12960036908826c445d41f9956b2e6bec0f37e1
Junior <jos.pd2019@gmail.com>
2023-05-12 07:17:31 -0300

    Creating Service For Password encrypt

---
+++ lib/app/services/encrypter/encrypt_service.dart
@@ -0,0 +1,17 @@
+import 'package:cash_helper_app/app/services/crypt_serivce.dart';
+
+class EncryptService implements ICryptService {
+  
+  @override
+  bool checkHashCode(String source, String hashCode) {
+    // TODO: implement checkHashCode
+    throw UnimplementedError();
+  }
+
+  @override
+  String generateHash(String source) {
+    // TODO: implement generateHash
+    throw UnimplementedError();
+  }
+  
+}
