bc1b3a9a9aff0abcf149afd73384b1b05d7be9a9
Jr_Silva_Dev <jos.pd2019@gmail.com>
2023-05-28 10:56:49 -0300

    Starting Widgets Drawing

---
+++ lib/app/modules/annotations_module/presenter/pages/annotation_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnnotationPage extends StatefulWidget {
+  const AnnotationPage({Key? key}) : super(key: key);
+
+  @override
+  State<AnnotationPage> createState() => _AnnotationPageState();
+}
+
+class _AnnotationPageState extends State<AnnotationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
